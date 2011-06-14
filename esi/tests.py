09f59a6deb06fac1303e6dd8f24d6ca1a34a6d50
Mike <mrfunyon@gmail.com>
2011-06-13 19:19:43 -0700

    corrected errors in tests

--- esi/tests.py
+++ esi/tests.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
 from django.test import TestCase, Client
+from django.template import Template, Context, RequestContext
 from esi.views import esi, get_object
 
 
@@ -19,21 +20,21 @@ class EsiTest(TestCase):
             'app_label': self.user._meta.app_label,
             'model_name': self.user._meta.module_name,
             'object_id': self.user.pk,
-            'timeout': 900,
+            'timeout': 1200,
             'template_name': 'includes/lists/auth.user.html'
         }
     
     def test_esi_templatetag(self):
         template = """
         {% load esi %}
-        {% esi for object path 'includes/lists/' timeout 1200 %}
+        {% esi for object path 'includes/lists' timeout 1200 %}
         """
         t = Template(template)
         c = Context({"object": self.user})
         rendered = t.render(c).strip()
-        args = kwargs.copy()
+        args = self.kwargs.copy()
         del(args['template_name'])
-        args.update({'template_dir':'includes/lists/'})
+        args.update({'template_dir':'includes/lists'})
         url = reverse('esi_list',kwargs=args)
         
         comparison = """<esi:include src="%s" />"""%(url)
@@ -43,27 +44,27 @@ class EsiTest(TestCase):
         """
         Tests that get_object returns an object and the correct model for that object.
         """
-        object, model = get_object(kwargs['app_label'], kwargs['model_name'], kwargs['object_id'])
+        object, model = get_object(self.kwargs['app_label'], self.kwargs['model_name'], self.kwargs['object_id'])
         self.assertEqual(object, self.user)
         self.assertEqual(model, User)
     
     def test_esi_view(self):
         """
         tests that the esi view sets the timeout cache header properly and renders the template correctly.
-        esi(request, app_label=None, model_name=None, object_id=None, timeout=900, template_name=None, template_dir=None)
+        esi(request, app_label=None, model_name=None, object_id=None, timeout=1200, template_name=None, template_dir=None)
         """
         client = Client()
         url = reverse('esi',kwargs=self.kwargs)
         r = client.get(url)
         age = int(r._headers['cache-control'][1].split("=")[1])
-        self.assertEqual(timeout, age)
+        self.assertEqual(self.kwargs['timeout'], age)
         
         # render the template and compare them.
-        t = loader.select_template([kwargs['template'],])
+        t = loader.select_template([self.kwargs['template_name'],])
         context = {
             'object': self.user,
         }
-        c = RequestContext(c.request, context)
+        c = RequestContext(r.request, context)
         rendered = t.render(c)
         
         self.assertEqual(r.content, rendered)
